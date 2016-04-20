13f6c83ebae16a80cd58e6405ba34b849e294e09
prateeklal <lalprateek01@gmail.com>
2016-04-20 09:00:22 +0530

    Added Services and Routing

--- webapp/flapper-news/app.js
+++ webapp/flapper-news/app.js
@@ -1,5 +1,5 @@
-var app = angular.module('flapperNews', ['ui-router']);
-app.factory('posts', [function() {
+var app = angular.module('flapperNews', ['ui.router']);
+app.factory('posts', [function(){
   var o = {
     posts: []
   };
@@ -8,21 +8,18 @@ app.factory('posts', [function() {
 app.controller('MainCtrl', [
 '$scope',
 'posts',
-function($scope,posts){
-  $scope.posts = posts.posts;
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+function($scope, posts){
+  $scope.posts = posts.posts[$stateParams.id];
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+         {author: 'Joe', body: 'Cool post!', upvotes: 0},
+         {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+       ]
     });
     $scope.title = '';
     $scope.link = '';
@@ -30,4 +27,38 @@ function($scope,posts){
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
   };
+}]);
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope,$stateParams,posts) {
+    $scope.addComment = function() {
+      if ($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    }
+  }]);
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      controller: 'PostsCtrl'
+      templateUrl: '/posts.html',
+    });
+
+  $urlRouterProvider.otherwise('home');
 }]);
